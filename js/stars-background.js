14ef136362f7e7031de5a2fbc32b1bed68928418
Angel Quintero <angelqv2014@gmail.com>
2025-08-19 22:09:16 -0400

    background & header
    added stars background, scanlines effect and header (visuals  only)

---
+++ js/stars-background.js
@@ -0,0 +1,192 @@
+﻿
+const STAR_COLOR = "#ffffff";
+const STAR_SIZE = 3;
+const STAR_MIN_SCALE = 0.2;
+const STAR_TAIL_SIZE = 0.5;
+const STAR_MOVE_MULTIPLIER = 0.5;
+const OVERFLOW_THRESHOLD = 50;
+const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8;
+
+const canvas = document.querySelector("canvas"),
+    context = canvas.getContext("2d");
+
+let scale = 1, // device pixel ratio
+    width,
+    height;
+
+let stars = [];
+let pointerX, pointerY;
+let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+let touchInput = false;
+
+generateStars();
+resize();
+step();
+
+window.onresize = resize;
+window.onmousemove = onMouseMove;
+window.ontouchmove = onTouchMove;
+document.onmouseleave = onMouseLeave;
+document.ontouchend = onMouseLeave;
+
+function generateStars() {
+    for (let i = 0; i < STAR_COUNT; i++) {
+        stars.push({
+            x: 0,
+            y: 0,
+            z: STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE)
+        });
+    }
+}
+
+function placeStar(star) {
+    star.x = Math.random() * width;
+    star.y = Math.random() * height;
+}
+
+function recycleStar(star) {
+    let direction = "z";
+
+    let vx = Math.abs(velocity.x),
+        vy = Math.abs(velocity.y);
+
+    if (vx > 1 || vy > 1) 
+    {
+        let axis;
+
+        if (vx > vy) 
+        {
+            axis = Math.random() < vx / (vx + vy) ? "horizontal" : "vertical";
+        } 
+        else 
+        {
+            axis = Math.random() < vy / (vx + vy) ? "vertical" : "horizontal";
+        }
+
+        if (axis === "horizontal") 
+        {
+            direction = velocity.x > 0 ? "left" : "right";
+        } 
+        else 
+        {
+            direction = velocity.y > 0 ? "top" : "bottom";
+        }
+    }
+
+    star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
+
+    if (direction === "z") {
+        star.z = 0.1;
+        star.x = Math.random() * width;
+        star.y = Math.random() * height;
+    } else if (direction === "left") {
+        star.x = -OVERFLOW_THRESHOLD;
+        star.y = height * Math.random();
+    } else if (direction === "right") {
+        star.x = width + OVERFLOW_THRESHOLD;
+        star.y = height * Math.random();
+    } else if (direction === "top") {
+        star.x = width * Math.random();
+        star.y = -OVERFLOW_THRESHOLD;
+    } else if (direction === "bottom") {
+        star.x = width * Math.random();
+        star.y = height + OVERFLOW_THRESHOLD;
+    }
+}
+
+function resize() {
+    scale = window.devicePixelRatio || 1;
+
+    width = window.innerWidth * scale;
+    height = window.innerHeight * scale;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    stars.forEach(placeStar);
+}
+
+function step() {
+    context.clearRect(0, 0, width, height);
+    update();
+    render();
+    requestAnimationFrame(step);
+}
+
+function update() {
+    velocity.tx *= STAR_MOVE_MULTIPLIER;
+    velocity.ty *= STAR_MOVE_MULTIPLIER;
+
+    velocity.x += (velocity.tx - velocity.x) * 0.8;
+    velocity.y += (velocity.ty - velocity.y) * 0.8;
+
+    stars.forEach((star) => {
+        star.x += velocity.x * star.z;
+        star.y += velocity.y * star.z;
+
+        star.x += (star.x - width / 2) * velocity.z * star.z;
+        star.y += (star.y - height / 2) * velocity.z * star.z;
+        star.z += velocity.z;
+
+        // recycle when out of bounds
+        if (star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD ||
+            star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD) {
+            recycleStar(star);
+        }
+    });
+}
+
+function render() {
+    stars.forEach((star) => {
+        context.beginPath();
+        context.lineCap = "round";
+        context.lineWidth = STAR_SIZE * star.z * scale;
+        context.globalAlpha = 0.2 + 0.5 * Math.random();
+        context.strokeStyle = STAR_COLOR;
+
+        context.beginPath();
+        context.moveTo(star.x, star.y);
+
+        let tailX = velocity.x * STAR_TAIL_SIZE,
+            tailY = velocity.y * STAR_TAIL_SIZE;
+
+        // stroke() wont work on an invisible line
+        if (Math.abs(tailX) < 0.1) tailX = 0.1;
+        if (Math.abs(tailY) < 0.1) tailY = 0.1;
+
+        context.lineTo(star.x + tailX, star.y + tailY);
+        context.stroke();
+    });
+}
+
+function movePointer(x, y) {
+    if (typeof pointerX === "number" && typeof pointerY === "number") 
+    {
+        let ox = x - pointerX,
+            oy = y - pointerY;
+
+        velocity.tx = velocity.tx + (ox / 8) * scale * (touchInput ? 1 : -1);
+        velocity.ty = velocity.ty + (oy / 8) * scale * (touchInput ? 1 : -1);
+    }
+
+    pointerX = x;
+    pointerY = y;
+}
+
+/// For desktop input
+function onMouseMove(event) {
+    touchInput = false;
+    movePointer(event.clientX, event.clientY);
+}
+
+/// For mobile input
+function onTouchMove(event) {
+    touchInput = true;
+    movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
+    event.preventDefault();
+}
+
+function onMouseLeave() {
+    pointerX = null;
+    pointerY = null;
+}
